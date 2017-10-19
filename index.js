360af3d84beb21873c22b3ae31a6ad0f93deec88
dean_neal <venomsunset@gmail.com>
2017-10-19 16:15:38 +0300

    remove folder if exists

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var rimraf = require('rimraf');
+const path = require('path');
+var mkdirp = require('mkdirp');
 
 // Delete the 0 and 1 argument (node and script.js)
 var args = process.argv.splice(process.execArgv.length + 2);
 // console.log(args);
 
-
 // The absolute path of the new file with its name
-
 
 if(args[0] === 'g') {
 	if(args[1]) {
 		var dir = args[1];
 
-		rimraf(dir, function () { console.log('done'); });
-		fs.mkdirSync(dir);
-		// if (!fs.existsSync(dir)){
-		//     fs.mkdirSync(dir);
-		// }
+		if (fs.existsSync(dir)){
+			console.log('done');
+			rimraf(dir, function () {
+				 createComponent(dir);
+			});
+		} else {
+			createComponent(dir);
+		}
 
-		var filepath = args[1] + '/' + args[2] + '.component';//"tmp/test.component.js";
-		console.log(filepath);
 
-		getComponent('lib/component.js', (fileContent)=>{
-			  fs.writeFile(filepath + '.js', fileContent, (err) => {
-			      if (err) throw err;
-			  }); 
-
-			  fs.writeFile(filepath + '.html', '', (err) => {
-			      if (err) throw err;
-			  }); 
-			 // console.log('Component is created');
-		});
 	}
 }
 
@@ -44,4 +35,28 @@ function getComponent(path, callback) {
 	  
 	  callback(data);
 	});
+}
+
+
+function createComponent(dir) {
+	mkdirp(dir, function (err) {
+    if (err) {
+   		 console.error(err)	
+    } else {
+    	console.log('pow!')
+
+    	var filepath = args[1] + '/' + args[2] + '.component';
+
+    	getComponent('lib/component.js', (fileContent)=>{
+    		  fs.writeFile(filepath + '.js', fileContent, (err) => {
+    		      if (err) throw err;
+    		  }); 
+
+    		  fs.writeFile(filepath + '.html', '', (err) => {
+    		      if (err) throw err;
+    		  }); 
+    		 console.log('Component is created');
+    	});
+    }
+});
 }
