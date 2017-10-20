48a2f7547f1746a738d52db096a35c8b443d0047
dean_neal <venomsunset@gmail.com>
2017-10-20 13:31:27 +0300

    refactoring

--- bin/component-generator.js
+++ bin/component-generator.js
@@ -1,68 +1,76 @@
 #!/usr/bin/env node
+
 const fs = require('fs');
 const rimraf = require('rimraf');
 const path = require('path');
 const mkdirp = require('mkdirp');
-const isGlobal = require ("is-global");
+const isGlobal = require("is-global");
 const globalModules = require('global-modules');
 
 let componentPath = '';
-
-if (isGlobal ()){
-  componentPath = globalModules + '/ac-cli/bin/component.js';
-}else{
-  componentPath = './bin/component.js';
-}
-console.log(componentPath, globalModules);
-
 // Delete the 0 and 1 argument (node and script.js)
 const args = process.argv.splice(process.execArgv.length + 2);
 
-if(args[0] === 'g') {
-	if(args[1]) {
-		const dir = args[1];
 
-		if (fs.existsSync(dir)){
-			rimraf(dir, function () {
-				 console.log('folder is removed');
-				 createComponent(dir);
-			});
-		} else {
-			createComponent(dir);
-		}
-
-
-	}
+if (isGlobal()) {
+   // fs.exists(`${globalModules}`, function(dir) {
+     //   if (!dir) throw 'node_modules does not exist';
+        // fs.readFile(globalModules + '/ac-cli/bin/component.js', 'utf8', function(err, data) {
+        //     init();
+        // });
+        componentPath = globalModules + '/ac-cli/bin/component.js';
+ //   });
+} else {
+    componentPath = './bin/component.js';
 }
 
-function getComponent(path, callback) {
-	fs.readFile(path, 'utf8', function (err,data) {
-	  if (err) {
-	    return console.log(err);
-	  }
-	  
-	  callback(data);
-	});
+
+
+if (args[0] === 'g') {
+    if (args[1]) {
+        const dir = args[1];
+
+        if (fs.existsSync(dir)) {
+            rimraf(dir, function() {
+                console.log('folder is removed');
+                createComponent(dir);
+            });
+        } else {
+            createComponent(dir);
+        }
+    }
 }
+
+
 
 
 function createComponent(dir) {
-	mkdirp(dir, function (err) {
-    if (err) {
-   		 console.error(err)	
-    } else {
-    	const filepath = args[1] + '/' + args[2] + '.component';
+    mkdirp(dir, function(err) {
+        if (err) {
+            console.error(err)
+        } else {
+            const filepath = args[1] + '/' + args[2] + '.component';
 
-    	getComponent(componentPath, (fileContent)=>{
-    		  fs.writeFile(filepath + '.js', fileContent, (err) => {
-    		      if (err) throw err;
-    		  }); 
+            getComponent(componentPath, (fileContent) => {
+                fs.writeFile(filepath + '.js', fileContent, (err) => {
+                    if (err) throw err;
+                });
 
-    		  fs.writeFile(filepath + '.html', '', (err) => {
-    		      if (err) throw err;
-    		  }); 
-    		 console.log('Component is created');
-    	});
-    }
-});
+                fs.writeFile(filepath + '.html', '', (err) => {
+                    if (err) throw err;
+                });
+                console.log('Component is created');
+            });
+        }
+    });
+}
+
+function getComponent(path, callback) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+
+        callback(data);
+    });
 }
